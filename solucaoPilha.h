ec85bd3d6e698cf5d1e1ae783e54f7be3f39ead5
juliana <107365569+JuApB@users.noreply.github.com>
2023-07-07 19:49:08 -0300

    Create solucaoPilha.h

---
+++ solucaoPilha.h
@@ -0,0 +1,11 @@
+#ifndef SOLUCAOPILHA_H
+#define SOLUCAOPILHA_H
+
+#include "labirinto.h"
+#include "listaEncadeada.h"
+#include "matriz.h"
+#include "percurso.h"
+
+Percurso *resolverPorDFS(Labirinto *);
+
+#endif // SOLUCAOPILHA_H
